7b5eb68ceb1cc2333f671227ecada9a5bb2d7a6b
Cauã Rinaldi <cauarinaldi451@gmail.com>
2022-12-13 09:05:29 -0300

    Add preconnect link

--- src/root.tsx
+++ src/root.tsx
@@ -5,7 +5,7 @@ import {
     ErrorBoundary,
     FileRoutes,
     Head,
-    Html,
+    Html, Link,
     Meta,
     Routes,
     Scripts,
@@ -23,7 +23,9 @@ export default function Root() {
                 <Title>Cauã Rinaldi</Title>
                 <Meta charset="utf-8"/>
                 <Meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <Meta name={"description"} content={"A personal portfolio website for Cauã Rinaldi, highlighting education, courses, programming languages, and projects."}></Meta>
+                <Meta name={"description"}
+                      content={"A personal portfolio website for Cauã Rinaldi, highlighting education, courses, programming languages, and projects."}></Meta>
+                <Link rel={"preconnect"} href={"https://fonts.gstatic.com"}></Link>
             </Head>
             <Body>
                 <Suspense>
