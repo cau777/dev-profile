e5adeeaacbc8c604fe3f4aa9ff24afebcf601f54
Cauã Rinaldi <cauarinaldi451@gmail.com>
2022-12-28 16:32:18 -0300

    Fix vite.gh-pages.config.ts

--- vite.gh-pages.config.ts
+++ vite.gh-pages.config.ts
@@ -1,9 +1,11 @@
 import solid from "solid-start/vite";
 import {defineConfig} from "vite";
+// @ts-ignore
+import ssg from "solid-start-static";
 
 export default defineConfig({
     plugins: [solid({
-        ssr: false,
+        adapter: ssg({})
     })],
     base: "/dev-profile/",
     build: {
