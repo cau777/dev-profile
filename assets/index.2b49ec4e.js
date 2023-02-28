56badd86d82f12f798a9f07407bd22a2612c2ffe
cau777 <cauarinaldi451@gmail.com>
2023-02-28 13:16:58 +0000

    Deploying to gh-pages from @ cau777/dev-profile@3d600d2c5a23257ebf0317540eaf31375d82d9db 🚀

---
+++ assets/index.2b49ec4e.js
@@ -0,0 +1,1 @@
+import{M as t}from"./Main.0a6b8bf8.js";import{c as o}from"./entry-client.9cea5671.js";function e(){return o(t,{lang:"pt"})}export{e as default};
--- assets/index.c8948e22.js
+++
@@ -1,1 +0,0 @@
-import{M as n}from"./Main.2b16f00d.js";import{c as o}from"./entry-client.2fdb1c8a.js";function t(){return o(n,{lang:"en"})}export{t as default};
