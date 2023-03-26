00517588aaa2dca6fb0baf2ac7f8a54e71189f9a
Cauã Rinaldi <cauarinaldi451@gmail.com>
2023-03-26 16:40:33 -0300

    Made the site just a redirect

--- assets/index.02d22333.js
+++
@@ -1,1 +0,0 @@
-import{M as t}from"./Main.eda054f1.js";import{c as o}from"./entry-client.73934f97.js";function e(){return o(t,{lang:"pt"})}export{e as default};
--- assets/index.1fb67a13.js
+++
@@ -1,1 +0,0 @@
-import{M as n}from"./Main.eda054f1.js";import{c as o}from"./entry-client.73934f97.js";function t(){return o(n,{lang:"en"})}export{t as default};
